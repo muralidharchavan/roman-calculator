1eb9a7183fcea64af1cf4f649650ed0a519fa619
Muralidhar Chavan <muralidhar.chavan@gmail.com>
2022-01-11 14:00:57 +0530

    Create ConsumerPactTest.java

---
+++ src/test/java/pact/ConsumerPactTest.java
@@ -0,0 +1,71 @@
+package pact;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "converter-mc", port = "1234", pactVersion = PactSpecVersion.V3)
+public class ConsumerPactTest {
+
+    private Map<String, String> headers = MapUtils.putAll(new HashMap<>(), new String[] {
+            "Content-Type", "text/plain"
+    });
+
+    @BeforeEach
+    public void setUp(MockServer mockServer) {
+        System.out.println("Mock server " + mockServer);
+        assertThat(mockServer, is(notNullValue()));
+    }
+
+    @Pact(consumer = "calculator-mc")
+    public RequestResponsePact convertToNumber(PactDslWithProvider builder) {
+        System.out.println("Generating pact file...");
+        return builder
+                .given("roman numeral")
+                .uponReceiving("convert to number")
+                .path("/to-number")
+                .method("GET")
+                .query("value=III")
+                .willRespondWith()
+                .headers(headers)
+                .status(200)
+                .body("3")
+                .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "convertToNumber")
+    void test(MockServer mockServer) throws IOException {
+        ClassicHttpResponse httpResponse = (ClassicHttpResponse) Request
+                .get(mockServer.getUrl() +
+                        "/to-number?value=III")
+                .execute().returnResponse();
+        System.out.println("Running consumer test success...");
+        assertThat(httpResponse.getCode(), is(equalTo(200)));
+        assertThat(IOUtils.toString(httpResponse.getEntity().getContent()),
+                is(equalTo("3")));
+
+    }
+}
